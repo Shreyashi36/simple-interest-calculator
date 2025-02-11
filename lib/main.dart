a76a3051b455d3d443d8fa5b13ea50505760f75a
Shreyashi Das <Shreyashi.D@Youngsoftindia.local>
2025-02-11 16:57:18 +0530

    made a basic list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/app_screens/home.dart';
+// import 'package:flutter_application_1/app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Exploring UI Widgets",
-    home: Home(),
-    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: 
+      AppBar(
+        title: Text("Long List"),
+        backgroundColor: Color.fromARGB(255, 65, 126, 239),
+      ),
+      body: getListView(),
+      drawerEnableOpenDragGesture: false,
+    )
     )
   );
+}
+
+Widget getListView(){
+  var listView = ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () => {
+          debugPrint("Landscape tapped")
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Windows"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+      ),
+    ],
+  );
+  return listView;
 }
